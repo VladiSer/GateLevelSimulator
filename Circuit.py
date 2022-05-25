74ff48abaaac021d6c9e3f9c663004bb478d267b
NivC94 <89209449+NivC94@users.noreply.github.com>
2022-05-25 14:45:36 +0300

    added memoization, not tested yet need to remove values from Signals

--- Circuit.py
+++ Circuit.py
@@ -6,12 +6,11 @@ class Circuit:
 
 	def __init__(self, name, gate_level_file): # TODO: get only file name
 		self._name = name
-		self._ingoing = {} # key = Gate, value = [input] signals
-		self._outgoing = {} # key = Gate, value = [output] signals
+		self._ingoing = {} # key = Dst Gate : value = { Src Gate : Signal }
+		self._outgoing = {} # key = Src Gate : value = { Dst Gate : Signal }
 		self._inputs = {} # key = input Port name, value = input Port
 		self._outputs = {} # key = output Port name, value = output Port
 		self.__parse_file(gate_level_file)
-
 
 
 	def __parse_file(self, gate_level_file):
@@ -32,8 +31,8 @@ class Circuit:
 		f.close()
 
 		for gate in gate_dict:
-			self._ingoing[gate] = []
-			self._outgoing[gate] = []
+			self._ingoing[gate] = {}
+			self._outgoing[gate] = {}
 
 		for gate1 in gate_dict:
 			inputs, outp = gate_dict[gate1][0:-1], gate_dict[gate1][-1]
@@ -45,8 +44,8 @@ class Circuit:
 					if inp == gate_dict[gate2][-1]:
 						found_output = True
 						sig = Signal(inp, gate2, gate1)
-						self._ingoing[gate1].append(sig)
-						self._outgoing[gate2].append(sig)
+						self._ingoing[gate1][gate2] = sig
+						self._outgoing[gate2][gate1] = sig
 						break
 				if not found_output:
 					try:
@@ -54,11 +53,11 @@ class Circuit:
 					except KeyError:
 						port = Port(inp, is_input=True)
 						self._inputs[inp] = port
-						self._outgoing[port] = []
+						self._outgoing[port] = {}
 
 					sig = Signal(inp, port, gate1)
-					self._ingoing[gate1].append(sig)
-					self._outgoing[port].append(sig)
+					self._ingoing[gate1][port] = sig
+					self._outgoing[port][gate1] = sig
 
 		for gate in gate_dict:
 			if not self._outgoing[gate]:
@@ -66,8 +65,11 @@ class Circuit:
 				port = Port(outp, is_input=False)
 				sig = Signal(outp, gate, port)
 				self._outputs[outp] = port
-				self._ingoing[port] = [sig]
-				self._outgoing[gate].append(sig)
+				try:
+					self._ingoing[port][gate] = sig
+				except KeyError:
+					self._ingoing[port] = {gate : sig}
+				self._outgoing[gate][port] = sig
 
 
 	def get_output_vectors_file(self, input_vector_file):
@@ -104,15 +106,27 @@ class Circuit:
 		f.close()
 		return self.get_outputs({inp[0]: inp[1] for inp in input_vectors})
 
+	def memoize_outputs(self, output_func):
+		outputs_dict = {} ## dict = Gate : out vector
+		def outputs(gate):
+			if gate not in outputs_dict:
+				outputs_dict[gate] = output_func(gate)
+			return outputs_dict[gate]
+
+		return outputs
+
+
+	@memoize_outputs
 	def __get_outputs_rec(self, vertex):
 		input_vector = []
-		for signal in self._ingoing[vertex]:
+		for signal in self._ingoing[vertex].values():
 			if not signal.value_vector:
 				# signal.value_vector = self.__get_outputs_rec(signal.get_opposite(vertex))
 				signal.set_val_vec(self.__get_outputs_rec(signal.get_opposite(vertex)))
 			input_vector.append(signal.value_vector)
 		a = vertex.get_output(input_vector)
 		return a
+
 # TODO: try to do memoization
 	def get_outputs(self, inputs):
 		if not inputs:
@@ -123,7 +137,7 @@ class Circuit:
 
 		for inp in self._inputs.values():
 			input_vector = inputs[inp.name]
-			for signal in self._outgoing[inp]:
+			for signal in self._outgoing[inp].values():
 				signal.value_vector = input_vector
 		a = {output.name: self.__get_outputs_rec(output) for output in self._outputs.values()}
 		return a
