341661f2264f76fede95149350592c8bcebee3d8
NivC94 <89209449+NivC94@users.noreply.github.com>
2022-05-28 12:27:13 +0300

    changed main to extract the file names from data dir and run for each pair of files

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from Circuit import Circuit
+import os
+
+input_files = []
+for file in os.listdir("data\\"):
+	if os.path.isfile(os.path.join("data\\", file)) and file.endswith(".gatelevel"):
+		vec_file_name = file.removesuffix(".gatelevel") + ".vec"
+		if vec_file_name in os.listdir("data\\"):
+			gatelevel_file_name = "data/" + file
+			vec_file_name = "data/" + vec_file_name
+			input_files.append((gatelevel_file_name, vec_file_name))
 
 
-for i in range(1, 5):
-	circuit = Circuit(f"data/cir{i}.gatelevel", f"Circuit{i}")
-	circuit.get_output_vectors_file(f"data/cir{i}.vec")
+for i in range(len(input_files)):
+	gatelevel_file, vec_file = input_files[i][0], input_files[i][1]
+	circuit = Circuit(gatelevel_file)
+	circuit.get_output_vectors_file(vec_file)
